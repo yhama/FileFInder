3d049a642156e22548d132cf866f2dfeef87cb30
yhama <sakata572@gmail.com>
2015-07-02 16:30:17 +0900

    refs #7 コマンドラインオプションで type オプションが指定された場合の処理を追加する.
    コマンドラインオプションで type オプションが指定された場合の処理を追加した.

--- src/main/java/com/github/FileFinder/Finder.java
+++ src/main/java/com/github/FileFinder/Finder.java
@@ -23,10 +23,43 @@ public class Finder
     }
 
     private boolean isTarget(File file)
+    {
+        boolean flag = true;
+        if (args.getName() != null)
+        {
+            flag &= checkTargetName(file, args.getName());
+        }
+		if (args.getType() != null)
+		{
+			flag &= checkTargetType(file, args.getType());
+		}
+        return flag;
+	}
+	
+	private boolean checkTargetName(File file, String pattern)
 	{
-        return true;
-    }
-
+		String name = file.getName();
+		return name.indexOf(pattern) >= 0;
+	}
+	
+	private boolean checkTargetType(File file, String type)
+	{
+		type = type.toLowerCase();
+		if (type.equals("d") || type.equals("directory"))
+		{
+			return file.isDirectory();
+		}
+		else if (type.equals("f") || type.equals("file"))
+		{
+			return file.isFile();
+		}
+		else if (type.equals("h") || type.equals("hidden"))
+		{
+			return file.isHidden();
+		}
+		return false;
+	}
+	
     private void traverse(List<String> list, File dir)
 	{
         if (isTarget(dir))
