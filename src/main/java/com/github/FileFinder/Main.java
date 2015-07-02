bfd00dcef4a4f866519b88b88a1cde6ceee4d9a2
okuba <yousuke5235@gmail.com>
2015-07-02 16:20:28 +0900

    refs #4 parseArgumentsメソッドの作成
    parseArgumentsメソッドを作成し、Mainのコンストラクタから呼び出した。

--- src/main/java/com/github/FileFinder/Main.java
+++ src/main/java/com/github/FileFinder/Main.java
@@ -1,11 +1,24 @@
 package com.github.finder;
 
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.CmdLineException;
+
 public class Main{
     public Main(String[] arguments)
     {
+        Args args = parseArguments(arguments);
+    }
+    private Args parseArguments(String[] arguments) throws CmdLineException
+    {
+        Args args = new Args();
+        CmdLineParser parser = new CmdLineParser(args);
+        parser.parserArgument(arguments);
+        
+        return args;
+        
         
     }
-    
+
     public static void main(String[] args)
     {
         //Mainクラスのコンストラクタを呼び出す
